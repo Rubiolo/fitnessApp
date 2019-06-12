cc5aa6dfd159d68e49170ecc08e54616fb499a36
Juan Francisco Fernández Pérez <Juan.Fernandez@lintra.de>
2019-06-12 15:16:14 +0200

    Card dinamica con Props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import fotoBloque1 from '../images/hombre-abs.jpg'
+//import fotoBloque1 from '../images/hombre-abs.jpg'
 import './styles/Card.css'
+import circulosImg from '../images/circles.png'
 
 class Card extends React.Component {
+
+    
+
     render() {
+        const { title, description, img, topColor, bottomColor } = this.props
         return (
-            <div className="card my-4 mx-auto Fitness-Card">
-                <div className="card">
+            <div className="card my-4 mx-auto Fitness-Card"
+            style={{
+                 
+                backgroundImage: `url(${circulosImg}), linear-gradient(to bottom, ${topColor}, ${bottomColor})`
+            }}
+            >
+                <div className="card-body">
                     <div className="row center ">
                         <div className="col-4 text-center align-self-center">
-                            <img src={fotoBloque1} className="Fitness-Img my-auto  " />
+                            <img src={img} alt="esto es un tio cuadrao" className="Fitness-Img my-auto  " />
                         </div>
-                        <div className="col-8 Fitness-Card-Info my-auto">
-                            <h1>Esta es el Título del primer bloque</h1>
-                            <p>Este sería el primer parrafo tras el título</p>
+                        <div className="col-8 Fitness-Card-Info my-auto mx-auto">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card/>,container);
+ReactDOM.render(<Card
+                    title="Esta es el Título desde index.js usando props"
+                    description="Este sería el primer parrafo tras el título"
+                    img="https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    topColor="#AD5CF0"
+                    bottomColor="#DBDCE2"
+                />,container);
 
