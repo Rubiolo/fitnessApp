382884e81ef4ac3cb44f1162535c3c9276d2a504
Juan Francisco Fernández Pérez <Juan.Fernandez@lintra.de>
2019-06-12 12:30:59 +0200

    first Card formatted

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import fotoBloque1 from '../images/hombre abs.jpg' 
+import fotoBloque1 from '../images/hombre-abs.jpg'
+import './styles/Card.css'
+
 class Card extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    <img src={fotoBloque1}/>
-                </div>
-                <div>
-                <h1>Esta es el Título del primer bloque</h1>
-                <p>Este sería el primer parrafo tras el título</p>
+            <div className="card my-4 mx-auto Fitness-Card">
+                <div className="card">
+                    <div className="row center">
+                        <div className="col-6 text-center ">
+                            <img src={fotoBloque1} className="Fitness-Img my-auto" />
+                        </div>
+                        <div className="col-6 Fitness-Card-Info my-auto">
+                            <h1>Esta es el Título del primer bloque</h1>
+                            <p>Este sería el primer parrafo tras el título</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
