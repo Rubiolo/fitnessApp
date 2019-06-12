1a098d55b1b2e61d882ba7d4182490b3400cecab
Juan Francisco Fernández Pérez <Juan.Fernandez@lintra.de>
2019-06-12 11:05:30 +0200

    1st Card Created (Img too Big)

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import fotoBloque1 from '../images/hombre abs.jpg' 
 class Card extends React.Component {
-    render(){
-        return <h1>Esta es mi Card!</h1>
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src={fotoBloque1}/>
+                </div>
+                <div>
+                <h1>Esta es el Título del primer bloque</h1>
+                <p>Este sería el primer parrafo tras el título</p>
+                </div>
+            </div>
+        )
     }
 }
 
