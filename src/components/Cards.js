b5bac1c3c7d6812e827a9bac3ec124cd1f0c0c7e
Juan Francisco Fernández Pérez <Juan.Fernandez@lintra.de>
2019-06-17 12:14:06 +0200

    Add Bienvenida.js & .css Add Pages with index.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,20 @@ import circulosImg from '../images/circles.png'
 
 class Card extends React.Component {
 
-    
+    constructor(props){
+        super(props)
+        this.state ={
+            image : 'https://images.pexels.com/photos/260352/pexels-photo-260352.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                image: 'https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            })
+        }, 4000);
+    }
 
     render() {
         const { title, description, img, topColor, bottomColor } = this.props
@@ -19,7 +32,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center ">
                         <div className="col-4 text-center align-self-center">
-                            <img src={img} alt="esto es un tio cuadrao" className="Fitness-Img my-auto  " />
+                            <img src={this.state.image} alt="esto es un tio cuadrao" className="Fitness-Img my-auto  " />
                         </div>
                         <div className="col-8 Fitness-Card-Info my-auto mx-auto">
                             <h1>{title}</h1>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Cards'
+import Welcome from './components/Bienvenida';
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card
-                    title="Esta es el Título desde index.js usando props"
-                    description="Este sería el primer parrafo tras el título"
-                    img="https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                    topColor="#AD5CF0"
-                    bottomColor="#DBDCE2"
-                />,container);
+ReactDOM.render(<div>
+    <Welcome 
+        username="Juanfran"
+    />
+    <Card
+        title="Esta es el Título desde index.js usando props"
+        description="Este sería el primer parrafo tras el título"
+        img="https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        topColor="#AD5CF0"
+        bottomColor="#DBDCE2"
+    />
+</div>, container);
 
