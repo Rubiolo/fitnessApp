149d1f3c8e07c6f6cca1be9b100716312c6e1520
Juan Francisco Fernández Pérez <Juan.Fernandez@lintra.de>
2019-06-26 12:17:54 +0200

    Trabajando con Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Exercises from '../pages/Exercises'
 import NotFound from '../pages/NotFound'
 import ExerciseNewContainer from '../pages/ExerciseNewContainer';
-
+import ExercisesContainer from '../pages/ExercisesContainer';
 
 const App = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/exercise" component={Exercises} />
             <Route exact path="/exercise/new" component={ExerciseNewContainer} />
+            <Route exact path="/exercise/container" component={ExercisesContainer} />
             <Route component={NotFound} />
         </Switch>
 
---
+++ src/pages/ExercisesContainer.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import FatalError from './500'
+import Exercises from './Exercises'
+import Loading from '../components/Loading';
+
+const ExercisesContainer = () => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchExercises = async () => {
+
+            try {
+                let res = await fetch('http://localhost:8000/api/exercises')
+                let data = await res.json()
+                setData(data)
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+                setError(error)
+            }
+        }
+        fetchExercises()
+    }, [])
+
+    if (loading) {
+        return <Loading />
+    }
+    if (error) {
+        return <FatalError />
+    }
+
+    return <Exercises
+        data={data}
+    />
+}
+export default ExercisesContainer
